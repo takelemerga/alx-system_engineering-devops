47c5f258ed1dd33d56735ab1983e915bd9ec0211
tolaa <mgamtaa@gmail.com>
2022-04-13 15:13:06 +0300

    returns info

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-'''
+"""
 Python script that returns information using REST API
-'''
+"""
 import requests
 from sys import argv
-
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com"
-    userId = argv[1]
-    user = requests.get(url + "users/{}".
-                        format(userId), verify=False).json()
-    alltask = requests.get(url + "todos?userId={}".
-                        format(userId), verify=False).json()
-    completed_tasks = []
-    for task in alltask:
-        if task.get('completed') is True:
-            completed_tasks.append(task.get('title'))
-    print("Employee {} is done with tasks({}/{}):".
-          format(user.get('name'), len(completed_tasks), len(alltask)))
-    print("\n".join("\t {}".format(task) for task in completed_tasks))
+if __name__ == "__main__":
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
