940db75ed3fe7f77af8ab9fc25de4b67093e0177
tolaa <mgamtaa@gmail.com>
2022-04-26 12:16:22 +0300

    return subsribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Reddit API module """
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+
+    headers = {'User-Agent': 'product-version-comment'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return (response.json().get("data").get("subscribers"))
+    return (0)
