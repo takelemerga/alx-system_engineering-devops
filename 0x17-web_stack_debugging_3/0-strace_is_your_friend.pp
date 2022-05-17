65636d8f47bedf341b0001b5d0260c931ebb43ed
tolaa <mgamtaa@gmail.com>
2022-05-17 09:05:46 +0300

    fix 500 error

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# find out why Apache is returning a 500 error, fix it and then automate it using Puppet
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
