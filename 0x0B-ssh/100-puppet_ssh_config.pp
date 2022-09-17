b85b83b3b6ffa749500333566ed0384c3abd3220
tolaa <mgamtaa@gmail.com>
2022-09-17 11:36:32 +0300

    config with puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,4 +1,6 @@
 # setup a client SSH configuration file so that we can connect to server without password
+# Your SSH client configuration must be configured to use the private key ~/.ssh/school
+# Your SSH client configuration must be configured to refuse to authenticate using a password
 
 exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
 path    => '/bin/'
