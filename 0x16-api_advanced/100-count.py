b46a905d03d49fd6644f1270c4911a91797b8ab6
tolaa <mgamtaa@gmail.com>
2022-11-01 16:40:11 +0300

    count word

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" recursive function that count word from Reddit API"""
+import requests
+import sys
+after = None
+count_dic = []
+
+
+def count_words(subreddit, word_list):
+    """parses the title of all hot articles, and prints a sorted count of given
+    keywords (case-insensitive, delimited by spaces) """
+    global after
+    global count_dic
+    headers = {'User-Agent': 'product-version-comment'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
