0259b34b3162a9754fe6b05d23dce7a3617de676
tolaa <mgamtaa@gmail.com>
2022-11-10 13:24:22 +0300

    request stack solved

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # fix our stack so that we get to 0 errors
-exec { 'file limit':
+exec { 'unlimit request':
   onlyif   => 'test -e /etc/default/nginx',
   command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
   provider => shell,
